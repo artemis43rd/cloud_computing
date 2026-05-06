cec67cc6cd62032681274bd1213b28dbf43c60e6
artemis43rd <43rd.artemis@mail.ru>
2026-05-06 11:08:13 +0300

    terraform-openstack: add version check

--- terraform-openstack/01-main.tf
+++ terraform-openstack/01-main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.54.1"
+    }
+  }
+}
+
 resource "openstack_networking_secgroup_v2" "sg" {
   name = "cherdantsev-sg-trfm"
 }
