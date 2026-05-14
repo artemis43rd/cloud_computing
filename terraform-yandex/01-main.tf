0060d88e4eb5789ed8f4b0a6ec46af239e2a4d6b
artemis43rd <43rd.artemis@mail.ru>
2026-05-14 13:22:58 +0300

    add lab5-yandex.yaml

--- terraform-yandex/01-main.tf
+++ terraform-yandex/01-main.tf
@@ -1,6 +1,3 @@
-# Follow instructions:
-# https://yandex.cloud/ru/docs/tutorials/infrastructure-management/terraform-quickstart
-
 terraform {
   required_providers {
     yandex = {
@@ -11,36 +8,28 @@ terraform {
 }
 
 data "external" "ya_auth" {
-  program = [
-    "bash", "yc_vars.sh",
-  ]
-  query = {
-    # You can pass something to STDIN of your program here, 
-    # but as per current version, the input will be given as JSON (map of string)
-  }
+  program = ["bash", "yc_vars.sh"]
 }
 
 provider "yandex" {
-  token = data.external.ya_auth.result.token
-  cloud_id =  data.external.ya_auth.result.cloud_id
-  folder_id =  data.external.ya_auth.result.folder_id
-  zone = "ru-central1-a"
+  token     = data.external.ya_auth.result.token
+  cloud_id  = data.external.ya_auth.result.cloud_id
+  folder_id = data.external.ya_auth.result.folder_id
+  zone      = "ru-central1-a"
 }
-
 
 resource "yandex_compute_disk" "cherdantsev-disk" {
   name     = "cherdantsev-disk"
   type     = "network-hdd"
-  zone     = "ru-central1-a"
   size     = "20"
-  image_id = "fd833v6c5tb0udvk4jo6" # ubuntu 22.04 LTS v20240325
+  image_id = "fd833v6c5tb0udvk4jo6" # Ubuntu 22.04
 }
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "cherdantsev-terraform"
-
+  name = "cherdantsev-yandex-vm"
+  platform_id = "standard-v3"
   resources {
-    cores  = 2 # 2, 4, 6, 8, 10, 12, 14, 16, 20, 24, 28, 32 allowed
+    cores  = 2
     memory = 2
   }
 
@@ -58,41 +47,17 @@ resource "yandex_compute_instance" "vm-1" {
   }
 }
 
-data "yandex_vpc_network" "existing" {
-  name = "default"
+resource "yandex_vpc_network" "network-1" {
+  name = "cherdantsev-net"
 }
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "cherdantsev-subnet"
   zone           = "ru-central1-a"
-  network_id     = data.yandex_vpc_network.existing.id
+  network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-resource "yandex_vpc_security_group" "group1" {
-  name        = "securuty-group-trfm"
-  network_id  = data.yandex_vpc_network.existing.id
-}
-
-resource "yandex_vpc_security_group_rule" "ssh-rule" {
-  security_group_binding = yandex_vpc_security_group.group1.id
-  direction              = "ingress"
-  description            = "Enable SSH"
-  v4_cidr_blocks         = ["0.0.0.0/0"]
-  port                   = 22
-  protocol               = "TCP"
-}
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-
-output "external_ip_address_vm_1" {
+output "server_ip" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
-
-output "servers" {
-  value = {
-    serverip = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-  }
 }
