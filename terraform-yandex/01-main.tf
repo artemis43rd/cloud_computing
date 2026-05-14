f03038980050d97d1f3b6b11eb0d54910e6f05d1
artemis43rd <43rd.artemis@mail.ru>
2026-05-14 13:45:43 +0300

    terraform-yandex: fix sec group

--- terraform-yandex/01-main.tf
+++ terraform-yandex/01-main.tf
@@ -22,7 +22,7 @@ resource "yandex_compute_disk" "cherdantsev-disk" {
   name     = "cherdantsev-disk"
   type     = "network-hdd"
   size     = "20"
-  image_id = "fd833v6c5tb0udvk4jo6" # Ubuntu 22.04 LTS
+  image_id = "fd833v6c5tb0udvk4jo6"
 }
 
 resource "yandex_compute_instance" "vm-1" {
@@ -39,9 +39,8 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   network_interface {
-    subnet_id          = yandex_vpc_subnet.subnet-1.id
-    nat                = true
-    security_group_ids = [yandex_vpc_security_group.lab5-sg.id] # Привязываем правила портов
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
   }
 
   scheduling_policy {
@@ -61,42 +60,7 @@ resource "yandex_vpc_subnet" "subnet-1" {
   name           = "cherdantsev-subnet"
   zone           = "ru-central1-a"
   network_id     = data.yandex_vpc_network.default-net.id
-  v4_cidr_blocks = ["192.168.101.0/24"] # Изменил на 101, чтобы не конфликтовать с существующими
-}
-
-# Настройка портов (Firewall)
-resource "yandex_vpc_security_group" "lab5-sg" {
-  name       = "cherdantsev-lab5-sg"
-  network_id = data.yandex_vpc_network.default-net.id
-
-  ingress {
-    protocol       = "TCP"
-    description    = "SSH"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 22
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "PostgreSQL"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 5432
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Kafka"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 9092
-  }
-
-  egress {
-    protocol       = "ANY"
-    description    = "Allow all outgoing"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 0
-    to_port        = 65535
-  }
+  v4_cidr_blocks = ["192.168.101.0/24"]
 }
 
 output "server_ip" {
